5469304fc568158c2654eda36417823db1dbbcbe
RosaitaW <55056093+RosaitaW@users.noreply.github.com>
2019-11-11 11:22:42 -0500

    Adjust Jumping logic, finish level design.

--- Week9HomeworkProject/Assets/Script/WASD_Script.cs
+++ Week9HomeworkProject/Assets/Script/WASD_Script.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class WASD_Script : MonoBehaviour
 {
     public float force = 0.1f;
+    //public float jumpforce = 0.1f;
     public KeyCode foward;
     public KeyCode back;
     public KeyCode Left;
     public KeyCode Right;
-    public KeyCode Jump;
+    //public KeyCode Jump;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -21,39 +22,43 @@ public class WASD_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool hasInput = false;
+         bool hasInput = false;
 
-        if (Input.GetKey(foward)) {
-            rb.velocity = new Vector3(0, 0, force);
-            hasInput = true;
-        }
-        if (Input.GetKey(back))
-        {
-            rb.velocity = new Vector3(0, 0, -force);
-            hasInput = true;
-        }
-        if (Input.GetKey(Left))
-        {
-            rb.velocity = new Vector3(-force, 0, 0);
-            hasInput = true;
-        }
-        if (Input.GetKey(Right))
-        {
-            rb.velocity = new Vector3(force, 0, 0);
-            hasInput = true;
-        }
-        if (Input.GetKeyDown(Jump))
-        {
-            if (transform.position.y <= 10f)
-            {
-                GetComponent<Rigidbody>().AddForce(Vector3.up * 3000);
-            }
-            hasInput = true;
-        }
+         if (Input.GetKey(foward))
+         {
+                rb.velocity = new Vector3(0, 0, force);
+                hasInput = true;
+         }
+         if (Input.GetKey(back))
+         {
+                rb.velocity = new Vector3(0, 0, -force);
+                hasInput = true;
+         }
+         if (Input.GetKey(Left))
+         {
+                rb.velocity = new Vector3(-force, 0, 0);
+                hasInput = true;
+         }
+         if (Input.GetKey(Right))
+         {
+                rb.velocity = new Vector3(force, 0, 0);
+                hasInput = true;
+         }
+         /*if (Input.GetKeyDown(Jump))
+         {
+             if (transform.position.y <= 10f)
+             {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * 5000);
+             }
+             hasInput = true;
+            }*/
+
         if (!hasInput)
         {
             //rb.velocity = new Vector3(0, 0, 0);
-            rb.velocity = rb.velocity * gravity;
+            rb.velocity = rb.velocity * 0.98f;
         }
+
     }
-}
+    }
+
