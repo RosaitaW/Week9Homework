a412ad57368c053001f6046be0a008c197b61cf4
RosaitaW <55056093+RosaitaW@users.noreply.github.com>
2019-11-10 22:38:01 -0500

    Added Movement script and materials.

--- Week9HomeworkProject/Assets/Script/WASD_Script.cs
+++ Week9HomeworkProject/Assets/Script/WASD_Script.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class WASD_Script : MonoBehaviour
 {
     public float force = 0.1f;
-    public KeyCode foward;
-    public KeyCode back;
+    //public KeyCode foward;
+    //public KeyCode back;
     public KeyCode Left;
     public KeyCode Right;
     public KeyCode Jump;
@@ -22,6 +22,7 @@ public class WASD_Script : MonoBehaviour
     void Update()
     {
         bool hasInput = false;
+
         if (Input.GetKey(foward)) {
             rb.velocity = new Vector3(0, 0, force);
             hasInput = true;
@@ -40,6 +41,19 @@ public class WASD_Script : MonoBehaviour
         {
             rb.velocity = new Vector3(force, 0, 0);
             hasInput = true;
+        }
+        if (Input.GetKeyDown(Jump))
+        {
+            if (transform.position.y <= 1.05f)
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * 2);
+            }
+            hasInput = true;
+        }
+        if (!hasInput)
+        {
+            //rb.velocity = new Vector3(0, 0, 0);
+            rb.velocity = rb.velocity * 0.98f;
         }
     }
 }
